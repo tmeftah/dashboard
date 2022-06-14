d1d2e7371c51ee635aeed326afd1051e379a4ddb
tmeftah <sudtechnics@gmail.com>
2022-06-14 14:05:36 +0200

    fix

--- static/sw.js
+++ static/sw.js
@@ -1,13 +1,13 @@
 var cacheName = "main_cache_v3";
 var filesToCache = [
   "/",
-  "./static/manifest.json",
-  "./static/app.js",
-  "./static/style.css",
-  "./static/login.css",
-  "./static/icon-512x512.png",
-  "./static/particles-7193862_960_720.webp",
-  "./static/meinAvatar.png",
+  "/static/manifest.json",
+  "/static/app.js",
+  "/static/style.css",
+  "/static/login.css",
+  "/static/icon-512x512.png",
+  "/static/particles-7193862_960_720.webp",
+  "/static/meinAvatar.png",
 ];
 
 /* Start the service worker and cache all of the app's content */
@@ -17,7 +17,7 @@ self.addEventListener("install", (e) => {
     (async () => {
       const cache = await caches.open(cacheName);
       console.log("[Service Worker] Caching all: app shell and content");
-      await cache.addAll(filesToCache);
+      // await cache.addAll(filesToCache);
       console.log("[Service Worker] Caching terminated");
     })()
   );
