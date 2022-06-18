87660dd3cbe4e3a57508b6833b683b993e548caa
tmeftah <sudtechnics@gmail.com>
2022-06-18 22:35:58 +0200

    fix: required field hidden not allow to submit

--- static/document.js
+++ static/document.js
@@ -8,24 +8,30 @@ document.addEventListener("DOMContentLoaded", function (event) {
   if (document_div) {
     var document_number = document.getElementById("document_number");
     var paymentmethod = document.getElementById("payment_id").value;
+    var due_date = document.getElementById("due_date");
     if (paymentmethod == 2 || paymentmethod == 3) {
       document_div.style.display = "block";
       document_number.removeAttribute("disabled");
+      due_date.required = true;
     } else {
       document_div.style.display = "none";
       document_number.setAttribute("disabled", "");
+      due_date.required = false;
     }
   }
 
   if (document_div1) {
     var document_number1 = document.getElementById("document_number1");
     var paymentmethod1 = document.getElementById("payment_id1").value;
+    var due_date1 = document.getElementById("due_date1");
     if (paymentmethod1 == 2 || paymentmethod1 == 3) {
       document_div1.style.display = "block";
       document_number1.removeAttribute("disabled");
+      due_date1.required = true;
     } else {
       document_div1.style.display = "none";
       document_number1.setAttribute("disabled", "");
+      due_date1.required = false;
     }
   }
 });
@@ -34,13 +40,16 @@ function ChangePaymentmethod() {
   var document_div = document.getElementById("document");
   var document_number = document.getElementById("document_number");
   var paymentmethod = document.getElementById("payment_id").value;
+  var due_date = document.getElementById("due_date");
 
   if (paymentmethod == 2 || paymentmethod == 3) {
     document_div.style.display = "block";
     document_number.removeAttribute("disabled");
+    due_date.required = true;
   } else {
     document_div.style.display = "none";
     document_number.setAttribute("disabled", "");
+    due_date.required = false;
   }
 }
 
@@ -48,12 +57,15 @@ function ChangePaymentmethod1() {
   var document_div = document.getElementById("document1");
   var document_number = document.getElementById("document_number1");
   var paymentmethod = document.getElementById("payment_id1").value;
+  var due_date1 = document.getElementById("due_date1");
 
   if (paymentmethod == 2 || paymentmethod == 3) {
     document_div.style.display = "block";
     document_number.removeAttribute("disabled");
+    due_date1.required = true;
   } else {
     document_div.style.display = "none";
     document_number.setAttribute("disabled", "");
+    due_date1.required = false;
   }
 }
