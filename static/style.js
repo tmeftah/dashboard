35d8c73a39d583cc65c9a64eacceb2cb9ec9cec1
tmeftah <sudtechnics@gmail.com>
2022-06-24 15:05:06 +0200

    change side bar behaviour

--- static/style.js
+++ static/style.js
@@ -1,8 +1,10 @@
 function show_sidebar() {
   var sidebar = document.getElementById("sidebar");
   var icon = document.getElementById("icon-sidebar-show-btn");
+  var btn = document.getElementById("sidebar-show-btn");
 
   sidebar.classList.toggle("show-sidebar");
+  btn.classList.toggle("expand");
 
   if (sidebar.classList.contains("show-sidebar")) {
     icon.classList.remove("bi-chevron-double-right");
