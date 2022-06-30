c2274615a14fa96c0a595db9835be9ca16d5ae83
tmeftah <sudtechnics@gmail.com>
2022-06-30 13:17:49 +0200

    fix: sidebar overlay

--- static/style.js
+++ static/style.js
@@ -2,6 +2,7 @@ function show_sidebar() {
   var sidebar = document.getElementById("sidebar");
   var icon = document.getElementById("icon-sidebar-show-btn");
   var btn = document.getElementById("sidebar-show-btn");
+  var overlay = document.getElementById("overlay");
 
   sidebar.classList.toggle("show-sidebar");
   btn.classList.toggle("expand");
@@ -9,8 +10,10 @@ function show_sidebar() {
   if (sidebar.classList.contains("show-sidebar")) {
     icon.classList.remove("bi-chevron-double-right");
     icon.classList.add("bi-chevron-double-left");
+    overlay.classList.add("active");
   } else {
     icon.classList.add("bi-chevron-double-right");
     icon.classList.remove("bi-chevron-double-left");
+    overlay.classList.remove("active");
   }
 }
