ee5a2da0b4558b0b7ad2dfe63e831e3b4dcf322d
tmeftah <sudtechnics@gmail.com>
2022-07-09 00:40:20 +0200

    fix INstall btn display/add install btn on app

--- app/static/app.js
+++ app/static/app.js
@@ -20,22 +20,22 @@ window.addEventListener("beforeinstallprompt", (e) => {
   // Update UI to notify the user they can add to home screen
   addBtn.style.display = "block";
   // demoBtn.style.display = "none";
+});
 
-  addBtn.addEventListener("click", (e) => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = "none";
-    // demoBtn.style.display = "block";
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
-      } else {
-        console.log("User dismissed the A2HS prompt");
-      }
-      deferredPrompt = null;
-    });
+addBtn.addEventListener("click", (e) => {
+  // hide our user interface that shows our A2HS button
+  addBtn.style.display = "none";
+  // demoBtn.style.display = "block";
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === "accepted") {
+      console.log("User accepted the A2HS prompt");
+    } else {
+      console.log("User dismissed the A2HS prompt");
+    }
+    deferredPrompt = null;
   });
 });
 
