65cf31eedcff049b4b8133093aab031c9c9801ce
tmeftah <sudtechnics@gmail.com>
2022-07-09 01:03:27 +0200

    sw for serving network first, cache second

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,5 +1,8 @@
-var cacheName = "main_cache_v5";
-var filesToCache = [
+// the cache version gets updated every time there is a new deployment
+const CACHE_VERSION = 10;
+const CURRENT_CACHE = `main-${CACHE_VERSION}`;
+
+var cacheFiles = [
   "/",
   "/login",
   "/static/manifest.json",
@@ -9,45 +12,64 @@ var filesToCache = [
   "/static/icon-512x512.png",
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener("install", (e) => {
-  console.log("[Service Worker] Install");
-  e.waitUntil(
-    (async () => {
-      const cache = await caches.open(cacheName);
-      console.log("[Service Worker] Caching all: app shell and content");
-      await cache.addAll(filesToCache);
-      console.log("[Service Worker] Caching terminated");
-    })()
-  );
-}); //
-
-/* Serve cached content when offline */
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
+// on activation we clean up the previously registered service workers
+self.addEventListener("activate", (evt) =>
+  evt.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CURRENT_CACHE) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
-  );
-});
+  )
+);
 
-///
+// on install we download the routes we want to cache for offline
+self.addEventListener("install", (evt) =>
+  evt.waitUntil(
+    caches.open(CURRENT_CACHE).then((cache) => {
+      return cache.addAll(cacheFiles);
+    })
+  )
+);
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches
-      .keys()
-      .then((keys) =>
-        Promise.all(
-          keys.map((key) => {
-            if (!cacheName.includes(key)) {
-              return caches.delete(key);
-            }
-          })
-        )
-      )
-      .then(() => {
-        console.log("New cache ready to handle fetches!");
-      })
+// fetch the resource from the network
+const fromNetwork = (request, timeout) =>
+  new Promise((fulfill, reject) => {
+    const timeoutId = setTimeout(reject, timeout);
+    fetch(request).then((response) => {
+      clearTimeout(timeoutId);
+      fulfill(response);
+      update(request);
+    }, reject);
+  });
+
+// fetch the resource from the browser cache
+const fromCache = (request) =>
+  caches
+    .open(CURRENT_CACHE)
+    .then((cache) =>
+      cache
+        .match(request)
+        .then((matching) => matching || cache.match("/offline/"))
+    );
+
+// cache the current page to make it available for offline
+const update = (request) =>
+  caches
+    .open(CURRENT_CACHE)
+    .then((cache) =>
+      fetch(request).then((response) => cache.put(request, response))
+    );
+
+// general strategy when making a request (eg if online try to fetch it
+// from the network with a timeout, if something fails serve from cache)
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    fromNetwork(evt.request, 10000).catch(() => fromCache(evt.request))
   );
+  evt.waitUntil(update(evt.request));
 });
