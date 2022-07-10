89cb4898377edd7e333f500a2ffc201c6cc6d8be
tmeftah <sudtechnics@gmail.com>
2022-07-10 21:26:29 +0200

    add unittest

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+from tests.basic import BasicsTestCase
+from flask import current_app
+
+
+class TestApp(BasicsTestCase):
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
+
+    def test_app_is_testing(self):
+        self.assertTrue(current_app.config["TESTING"])
